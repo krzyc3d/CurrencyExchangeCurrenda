221399e0c36e49145c4ea45576d27d27a6a73d82
krzyc3d <krzych3d@wp.pl>
2018-12-03 19:36:55 +0100

    init commit

---
+++ src/main/java/kap/currency/CurrencyExchangeCurrendaApplication.java
@@ -0,0 +1,12 @@
+package kap.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeCurrendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeCurrendaApplication.class, args);
+	}
+}
